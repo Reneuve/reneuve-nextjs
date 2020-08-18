68e6e52218066e1d3d604bac742a6a082ae7535f
Rafael Calvin <rafael@192.168.1.16>
2020-08-18 09:42:15 +0700

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function FirstPost() {
+    return (
+        <div>
+            <Link href="/">Home</Link>
+            <h1>First Post</h1>
+        </div>
+    )
+}
